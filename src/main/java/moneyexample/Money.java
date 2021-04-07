a750989df207b4a176f147fa0de9feb212db07aa
Magdalena Żaba <magdalenazaba@tlen.pl>
2021-04-07 11:00:34 +0200

    Get rid of redundant subclasses

--- src/main/java/moneyexample/Money.java
+++ src/main/java/moneyexample/Money.java
@@ -1,6 +1,6 @@
 package moneyexample;
 
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
 
@@ -10,24 +10,31 @@ abstract class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
 
-    abstract Money times(int multiplier);
+   Money times(int multiplier){
+        return new Money(amount* multiplier,currency) ;
+   }
 
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object ;
         return amount== money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
     }
 
     protected String currency(){
         return currency;
+    }
+
+    @Override
+    public String toString() {
+        return amount+" "+currency;
     }
 }
--- src/test/java/moneyexample/MoneyTest.java
+++ src/test/java/moneyexample/MoneyTest.java
@@ -17,16 +17,7 @@ public class MoneyTest {
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-
-    @Test
-    public void testFrancMultiplication(){
-        Money five = Money.franc(5);
-        assertEquals(five.times(2), Money.franc(10));
-        assertEquals(five.times(3), Money.franc(15));
     }
 
     @Test
@@ -34,5 +25,6 @@ public class MoneyTest {
         assertEquals("USD",Money.dollar(1).currency());
         assertEquals("CHF",Money.franc(1).currency());
     }
+
 
 }
