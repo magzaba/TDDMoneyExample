366caeed26d539628e6264cc3a4fad7232e3058e
Magdalena Żaba <magdalenazaba@tlen.pl>
2021-04-08 15:02:33 +0200

    Make Pair private

--- src/main/java/moneyexample/Bank.java
+++ src/main/java/moneyexample/Bank.java
@@ -3,7 +3,28 @@ package moneyexample;
 import java.util.HashMap;
 
 class Bank {
-    private HashMap<Pair,Integer> rates = new HashMap<>();
+    private final HashMap<Pair,Integer> rates = new HashMap<>();
+
+   private static class Pair {
+        private final String from;
+        private final String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            Pair pair =(Pair) object;
+            return from.equals(pair.from)&&to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
 
     Money reduce(Expression source, String to) {
         return source.reduce(this,to);
@@ -11,10 +32,10 @@ class Bank {
 
     public int rate(String from, String to) {
         if(from.equals(to)) return 1;
-        return rates.get(new Pair(from,to));
+        return rates.get(new Pair(from, to));
     }
 
     public void addRate(String from, String to, int rate) {
-    rates.put(new Pair(from,to),rate);
+    rates.put(new Pair(from, to),rate);
     }
 }
